94197f26f0e44dfe2417176ea3b901182ac72c66
Luis Villalpando <113930826+Levillalpand1@users.noreply.github.com>
2023-02-16 18:40:40 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# # remember to fork this and push to github'
+
+# f = open('employees.txt', 'a')
+# f.write("Daryle - lead worker")
+# # read gives us everything
+# # readline givs is the firtline
+# print(f.read())
+
+# f.close()
+
+# with open("text.txt", "r") as f:
+#   # f.read()
+# for line in f:
+#   print(line, end='')
+
+
+with open ('period.txt', 'w') as f:
+  f.write("this message is over")
